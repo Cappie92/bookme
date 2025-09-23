9c3d6242be8a98462ab9c51b3d62dc2b257aa5c2
Appointo Developer <admin@appointo.ru>
2025-09-23 13:37:34 +0300

    Remove Footer from UserAgreement to test if it's the source of duplication

--- frontend/src/pages/UserAgreement.jsx
+++ frontend/src/pages/UserAgreement.jsx
@@ -1,5 +1,4 @@
 import Header from "../components/Header"
-import Footer from "../components/Footer"
 
 export default function UserAgreement() {
   return (
@@ -16,7 +15,6 @@ export default function UserAgreement() {
           <p>Дата последнего обновления: 26 июня 2025 г.</p>
         </div>
       </main>
-      <Footer />
     </div>
   )
 } 
