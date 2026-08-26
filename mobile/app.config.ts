5787cc837c875de6892b3996f70f1e9c716f06f8
Sergey Devyatov <sergdevyatov@gmail.com>
2026-08-26 22:50:08 +0300

    chore: prepare Apple IAP production build 3

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -71,7 +71,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     ios: {
       supportsTablet: true,
       bundleIdentifier: 'com.dedato.app',
-      buildNumber: '2',
+      buildNumber: '3',
       associatedDomains,
       config: {
         usesNonExemptEncryption: false,
