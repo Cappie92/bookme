ce82c69d6462fa2e664e4d67b40cd4d25e7c71df
Sergey Devyatov <sergdevyatov@gmail.com>
2026-08-29 21:45:34 +0300

    chore: bump iOS build number to 2

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -71,7 +71,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     ios: {
       supportsTablet: true,
       bundleIdentifier: 'com.dedato.app',
-      buildNumber: '1',
+      buildNumber: '2',
       associatedDomains,
       config: {
         usesNonExemptEncryption: false,
